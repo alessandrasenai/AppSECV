d353cd13b86cbdea3f4b5e743039fb685dddd5ee
alessandra <alessandrasenai@outlook.com>
2018-10-03 19:12:44 -0300

    perfil adm
    incompleta

--- app/src/main/java/br/com/rafaelleme/senai/appsecv/MainActivity.java
+++ app/src/main/java/br/com/rafaelleme/senai/appsecv/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.rafaelleme.senai.appsecv;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,9 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void pagina2 (View view){
+
+        Intent intent = new Intent(this, SegundaActivity.class);
+        startActivity(intent);
     }
 }
-
 
 
 
