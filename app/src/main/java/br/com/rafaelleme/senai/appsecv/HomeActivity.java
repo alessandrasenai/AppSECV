62abbebf2b10f678bae7ce420633afdffc13d1c9
alessandra <alessandrasenai@outlook.com>
2018-10-18 19:13:41 -0300

    tela login
    menu lateral

--- app/src/main/java/br/com/rafaelleme/senai/appsecv/HomeActivity.java
+++ app/src/main/java/br/com/rafaelleme/senai/appsecv/HomeActivity.java
@@ -1,17 +1,39 @@
 package br.com.rafaelleme.senai.appsecv;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-
-
-
+        setUpToolbar();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
-}
+        private void setUpToolbar() {
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            if(item.getItemId() == android.R.id.home){
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+
+        }
+
