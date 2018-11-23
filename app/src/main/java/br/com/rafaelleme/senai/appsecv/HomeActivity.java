245bae2ef7cbdf8ee48bf397119b7763b40d77bc
alessandra <alessandrasenai@outlook.com>
2018-11-23 19:18:12 -0200

    tela login
    ajuda

--- app/src/main/java/br/com/rafaelleme/senai/appsecv/HomeActivity.java
+++ app/src/main/java/br/com/rafaelleme/senai/appsecv/HomeActivity.java
@@ -13,93 +13,89 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import static android.support.v7.app.AlertDialog.*;
+import static br.com.rafaelleme.senai.appsecv.R.id.action_bar;
 import static br.com.rafaelleme.senai.appsecv.R.id.itemUm;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-
-        return false;
-    }
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        setUpToolbar();
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle("Estatísticas");
+    public void mostrarajuda();{
 
-        navigationView = findViewById(R.id.navView);
-        navigationView.setNavigationItemSelectedListener(this);
+        {
+            LayoutInflater li = getLayoutInflater();
+            View view = li.inflate(R.layout.ajuda_dialog, null);
+            view.findViewById(R.id.edtTEXT1).setOnClickListener(new View.OnClickListener() {
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-    }
-
-
-    private void setUpToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-        private AlertDialog alerta;
-
-    private void setNavigationView() {
-
-    }
-
-    {
-
-        Builder builder = new Builder(this);
-        builder.setTitle("titulo");
-        builder.setMessage("digite");
-        Builder  = new builder ("ic_helpoutline", new OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                switch (MenuItem.OnMenuItemClickListener) {
-
-                    case itemUm:
-                        Toast.makeText(activity_main.this, "Clicou no item 1", Toast.LENGTH_SHORT).
-
-                                show();
-                        break;
-
-                    case R.id.itemDois:
-
-                        break;
-
-                    case R.id.itemTres:
-
-                        break;
-
-                    case R.id.itemQuatro:
-                        break;
-
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(HomeActivity.this, "ajuda", Toast.LENGTH_SHORT).show();
+                    mostrarajuda();
                 }
+            });
 
+        }
+        }
 
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.itemUm:
+                    Toast.makeText(this, "Abriu item 1", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.itemAjuda:
+
+                    break;
             }
 
+            return false;
+        }
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
+            setUpToolbar();
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Estatísticas");
+
+            navigationView = findViewById(R.id.navView);
+            navigationView.setNavigationItemSelectedListener(this);
+
+            drawerLayout = findViewById(R.id.drawer_layout);
+        }
+
+
+        private void setUpToolbar() {
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
         }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            if (item.getItemId() == android.R.id.home) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+            return super.onOptionsItemSelected(item);
+
+
+        }
+
+
+    }
+
+
